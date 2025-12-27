db807040e4bd50e2c220a7db9ed460239645a0c3
yiğit kandemir <kandemirryigit@gmail.com>
2025-12-27 16:19:47 +0300

    Add files via upload

---
+++ main48.asm
@@ -0,0 +1,35 @@
+;--------------------------------------
+;SHL (LOGICAL SHIFT RIGHT) PROGRAM
+; DISPLAY THE NUMBER ON THE SCREEN (0-9)
+;---------------------------------------
+
+
+data segment
+    num db 12  ; 00001100
+    result db ?
+data ends
+
+code segment
+assume cs:code,ds:data
+
+start:
+    mov ax,data
+    mov ds,ax
+    
+    mov al,num
+    shr al,1
+    mov result,al   ; result=00000110
+    
+    ;print result as decimal
+    mov dl,result
+    add dl,30h   ; Convert ASCII
+    mov ah,02h
+    int 21h
+    
+    mov ah,4Ch
+    int 21h
+    
+code ends
+end start
+    
+    
